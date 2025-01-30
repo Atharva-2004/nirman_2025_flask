469d328aad31042ba4bc85ec153b2345674968b8
Atharva Ajagekar <atharva7ajagekar@gmail.com>
2025-01-30 07:31:19 +0530

    handler

--- app.py
+++ app.py
@@ -71,6 +71,10 @@ def predict():
 
     except Exception as e:
         return render_template('index.html', error=str(e))
+    
+# Required for Vercel
+def handler(event, context):
+    return app(event, context)
 
 if __name__ == '__main__':
     app.run(debug=True)
